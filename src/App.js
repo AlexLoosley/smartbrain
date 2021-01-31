c8ed9c87558239d65040d75ab4f9ee47b9f085ad
Alexander Loosley <al_loosley@hotmail.com>
2021-01-31 20:17:09 +0000

    added ImageLink css file; added Rank component; added particles component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLink from "./Components/ImageLink/ImageLink";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-particles-js";
 import "./App.css";
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends Component {
   render() {
     return (
       <div classname="App">
+        <Particles className="particles" params={particlesOption} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLink />
         {/* { 
         <FaceRecognition />} */}
--- src/Components/ImageLink/ImageLink.js
+++ src/Components/ImageLink/ImageLink.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./ImageLink.css";
 
 const ImageLink = () => {
   return (
     <div>
-      <p className="f3 center">
+      <p className="f3   center">
         This magic brain will detect faces in your pictures! Give it a try!
       </p>
       <div className="center">
-        <div className="pa4 br3 shadow-5 center">
+        <div className="form pa4 br3 shadow-5 center">
           <input className="f4 pa2 w-70" />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
