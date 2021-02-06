dc6e05acb8b12b2cdfc8953d1acb522cca4ea494
Alexander Loosley <al_loosley@hotmail.com>
2021-02-06 22:35:36 +0000

    added deprecated Clarifai API for facial recognition to App.js and added onInputChange and on ButtonSubmit to ImageLink.js

--- src/App.js
+++ src/App.js
@@ -5,28 +5,58 @@ import ImageLink from "./Components/ImageLink/ImageLink";
 import Rank from "./Components/Rank/Rank";
 import Particles from "react-particles-js";
 import "./App.css";
+import Clarifai from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "c28cb2b6c84a4de6997a526c83d9db8d",
+});
 
 const particlesOption = {
   particles: {
     number: {
-      value: 50,
+      value: 100,
       density: {
         enable: true,
-        value_area: 800,
+        value_area: 700,
       },
     },
   },
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        "https://upload.wikimedia.org/wikipedia/commons/a/a9/Tom_Hanks_TIFF_2019.jpg"
+      )
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
-      <div classname="App">
+      <div className="App">
         <Particles className="particles" params={particlesOption} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLink />
+        <ImageLink
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* { 
         <FaceRecognition />} */}
       </div>
